38be3a93ecffc979a2cad3fb1f3aacccb3d9c1e0
Vikas Dwivedi <vikas.nice@gmail.com>
2024-01-20 17:03:12 +0530

    Added Todo list

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap';
+import {useState} from 'react';
+
+const data = [
+  {
+    title: 'Item 1',
+    id: 1
+  },
+  {
+    title: 'Item 2',
+    id: 2
+  },
+  {
+    title: 'Item 3',
+    id: 3
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(data);
+  const [newTodo, setNewTodo] = useState('');
+  const addTodo = () => {
+    // const newArray = oldArray + new Element
+    const newArray = [...todos];
+    newArray.push({
+      id: todos.length,
+      title: newTodo
+    })
+    setTodos(newArray);
+
+    setTodos([...todos, {
+      id: todos.length,
+      title: newTodo
+    }]);
+    setNewTodo('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+        {
+          todos.map((todo)=>
+          <div key={todo.id} className="p-1 border d-flex justify-content-between">
+            <span>{todo.title}</span><Button>X</Button>
+          </div>)
+        }
+        <div className="p-1 border d-flex justify-content-end">
+          <input type="text" placeholder="Enter todo..." onChange={(e) => setNewTodo(e.target.value)} value={newTodo}/>
+          <Button className="btn-success" onClick={addTodo}>+</Button>
+        </div>
     </div>
   );
 }
